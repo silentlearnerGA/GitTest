06c79eae6bf6bbedcdaa297cc12e07244cdbd64f
Md. Golam Azam <md.golam@Mds-iMac.local>
2017-06-19 22:55:57 -0700

    undo commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -19,9 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
-    }
+
 
 }
 
